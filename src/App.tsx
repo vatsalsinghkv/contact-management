b0c2390a6c44463f276a76bbe8cb16b5e9a3ad21
vatsal <vatsalsinghkv@gmail.com>
2023-05-08 13:56:53 +0530

    add: react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import { Chart, Contact, NotFound } from './pages';
+
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-sky-500 text-5xl'>Hello World!</h1>
+      <Routes>
+        <Route path='/' element={<Contact />} />
+        <Route path='/chart' element={<Chart />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Chart from './Chart';
+import Contact from './Contact';
+import NotFound from './NotFound';
+
+export { Chart, Contact, NotFound };
